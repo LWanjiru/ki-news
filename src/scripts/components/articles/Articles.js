b5ddd1826e0092735c9df84ea1e5efe0769f4891
LWanjiru <linnette.wanjiru@andela.com>
2017-05-15 01:14:26 +0200

    update package.json

--- src/scripts/components/articles/Articles.js
+++ src/scripts/components/articles/Articles.js
@@ -16,12 +16,11 @@ export default class Articles extends React.Component {
     return (
       <div id="main" className="stories">
         <a href="/"><h1>Headlines</h1></a>
-        <iframe href="/Articles" name="iframe_a" />
         {this.props.articles && this.props.articles.map((article, index) => (
           <div className="art" key={index}>
             <ul>
               <div className="story-pics">
-                <a href={article.url} rel="noopener noreferrer" target="iframe_a" >
+                <a href={article.url} rel="noopener noreferrer" target="_blank" >
                   <h3><img src={article.urlToImage} alt="story" />{article.title}</h3></a>
                 {article.description}
               </div>
