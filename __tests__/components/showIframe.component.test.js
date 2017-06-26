ecd7b34f86eeeacf84cc51fe226cc70fcfc0a2fb
LWanjiru <linnette.wanjiru@andela.com>
2017-06-26 22:53:21 +0200

    remove unnecessary files from git

--- __tests__/components/showIframe.component.test.js
+++ __tests__/components/showIframe.component.test.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ShowIframe from './../../src/scripts/components/showIframe';
 
-it('renders without crashing', () => {
+it('renders correctly', () => {
   shallow(<ShowIframe />);
 });
