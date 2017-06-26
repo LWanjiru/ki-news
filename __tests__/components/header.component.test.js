1caa78170186ff60fba4c84c2eb9ff6aba81a491
LWanjiru <linnette.wanjiru@andela.com>
2017-06-26 22:55:07 +0200

    remove unnecessary files from git

--- __tests__/components/header.component.test.js
+++ __tests__/components/header.component.test.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './../../src/scripts/components/Header';
 
-it('renders without crashing', () => {
+it('renders the header component', () => {
   shallow(<Header />);
 });
