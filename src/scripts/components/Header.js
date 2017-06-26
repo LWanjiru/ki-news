abd815d56bb59306b72c53c4d4a88f2f10381d1d
LWanjiru <linnette.wanjiru@andela.com>
2017-06-27 01:40:38 +0200

    fix heroku login bug

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -3,6 +3,9 @@ import Login from './Login';
 
 const Header = () => {
   const header = 'KI-All News';
+  const imgWidth = 40;
+  const rightMargin = 10;
+  const fontHeader = { font: 35 };
   return (
     <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
       <div className="navbar-toggler-right">
@@ -11,10 +14,12 @@ const Header = () => {
       <a className="navbar-brand" href="/">
         <img
           src="./../../../../public/logo.jpg"
+          width={imgWidth}
+          margin-right={rightMargin}
           className="card-inline-block align-top"
           alt="page logo"
         />
-        <strong>{header}</strong>
+        <strong style={{ fontHeader }}>{header}</strong>
       </a>
     </nav>
   );
