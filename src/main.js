9f0794c9c4e0ea4c627c0f7d202fec7c461e0c88
LWanjiru <linnette.wanjiru@andela.com>
2017-06-27 01:02:18 +0200

    fix heroku login bug

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './scripts/components/App';
 
-require('./../public/stylesheets/index.scss');
-
 render(<App />, document.getElementById('root'));
