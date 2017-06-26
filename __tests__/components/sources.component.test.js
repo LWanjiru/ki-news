cb265a17ea1995dc3536d609b37a067beb331600
LWanjiru <linnette.wanjiru@andela.com>
2017-06-26 22:31:37 +0200

    remove unnecessary files from git

--- __tests__/components/sources.component.test.js
+++ __tests__/components/sources.component.test.js
@@ -6,11 +6,3 @@ import SourceList from './../../src/scripts/components/Sources';
 it('renders without crashing', () => {
   shallow(<SourceList />);
 });
-it('renders sources', () => {
-  const sources = ['source1'];
-  const wrapper = mount(
-    <SourceList source={sources} />,
-  );
-  const a = wrapper.find('h5');
-  expect(a.text()).toBe('source1');
-});
