f450df677a535a02a47bc988f2b45e801af60391
LWanjiru <linnette.wanjiru@andela.com>
2017-06-27 00:18:55 +0200

    fix heroku login bug

--- src/scripts/components/Login.js
+++ src/scripts/components/Login.js
@@ -5,8 +5,7 @@ export default class Login extends Component {
  // generate a token when a user signs in
   responseGoogle(googleUser) {
     const idToken = googleUser.getAuthResponse();
-    console.log(idToken);
-    // return (idToken);
+    return (idToken);
   }
 
   render() {
@@ -15,7 +14,7 @@ export default class Login extends Component {
         <GoogleLogin
           socialId="1054136645395-cf4o0i0er8treqp01til7t28a7ngcqg9.apps.googleusercontent.com"
           class="btn btn-danger"
-          scope="https://www.googleapis.com/auth/appstate"
+          scope="https://www.googleapis.com/auth/plus.login"
           responseHandler={this.responseGoogle}
           buttonText="Login with Google+"
         />
