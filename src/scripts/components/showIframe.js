d0c367c52c400874d9e3620bbe424353dfa0a399
linnettewanjiru <linnette.wanjiru@andela.com>
2018-05-29 15:35:41 +0300

    [Chore #157842165] fix hound issue

--- src/scripts/components/showIframe.js
+++ src/scripts/components/showIframe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ShowIframe = () => (
-  <div className="mt-3 embed-responsive embed-responsive-16by9" id="i-frame">
+  <div className="mt-3 embed-responsive embed-responsive-16by9 i-frame" >
     <iframe className="border border-warning rounded" name="iframe_a" />
   </div>
 );
